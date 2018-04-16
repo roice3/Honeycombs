9341e7811541d461d1b1c3a156e33455756aafc3
Roice Nelson <roice3@gmail.com>
2018-04-15 19:34:33 -0500

    Add some interesting Euclidean models.

---
+++ code/R3/R3.Core/Geometry/EuclideanModels.cs
@@ -0,0 +1,28 @@
+﻿namespace R3.Geometry
+{
+	using R3.Geometry;
+
+	public enum EuclideanModel
+	{
+		Isometric,
+		Conformal,
+		Disk,
+		UpperHalfPlane,
+	}
+
+	public class EuclideanModels
+	{
+		public static Vector3D DiskToIsometric( Vector3D v )
+		{
+			// ZZZ - Check that this is correct (it's quite close if not!)
+			return SphericalModels.StereoToGnomonic( v );
+		}
+
+		public static Vector3D UpperHalfPlaneToIsometric( Vector3D v )
+		{
+			v = HyperbolicModels.UpperToPoincare( v );
+			v = SphericalModels.StereoToGnomonic( v );
+			return v;	
+		}
+	}
+}
